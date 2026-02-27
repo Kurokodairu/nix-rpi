2e28a933f4dd779878623dd3eb7dc2419a7082ae
Jørgen <jsvendsli@gmail.com>
2026-02-27 22:57:43 +0100

    rpi repo

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    nixos-raspberrypi.url = "github:nvmd/nixos-raspberrypi/main";
 
     sops-nix = {
       url = "github:Mic92/sops-nix";
@@ -11,44 +11,46 @@
     };
   };
 
+  nixConfig = {
+    extra-substituters = [
+      "https://nixos-raspberrypi.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
+    ];
+  };
+
   outputs = {
     self,
     nixpkgs,
-    nixos-hardware,
+    nixos-raspberrypi,
     sops-nix,
     ...
   }:
-  let
-    baseModules = [
-      nixos-hardware.nixosModules.raspberry-pi-4
-      sops-nix.nixosModules.sops
-      { _module.args.self = self; }
-      ./configuration.nix
-      ./hardware.nix
-      ./modules/avahi.nix
-      ./modules/ssh.nix
-      ./modules/users.nix
-      ./modules/wireguard.nix
-      ./modules/packages.nix
-      ./modules/auto-deploy.nix
-    ];
-  in
   {
-    # Running system (post-bootstrap, sops secrets)
-    nixosConfigurations.rpi = nixpkgs.lib.nixosSystem {
-      system = "aarch64-linux";
-      modules = baseModules ++ [
-        ./modules/networking.nix
-      ];
-    };
+    nixosConfigurations.rpi =
+      nixos-raspberrypi.lib.nixosSystem {
+        modules = [
+          { _module.args.self = self; }
 
-    # SD card image (pre-bootstrap, hardcoded WiFi)
-    nixosConfigurations.rpi-image = nixpkgs.lib.nixosSystem {
-      system = "aarch64-linux";
-      modules = baseModules ++ [
-        "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
-        ./modules/bootstrap-image.nix
-      ];
-    };
+          # RPi4 hardware
+          nixos-raspberrypi.nixosModules.raspberry-pi-4.base
+          nixos-raspberrypi.nixosModules.raspberry-pi-4.display-vc4
+
+          # Sops
+          sops-nix.nixosModules.sops
+
+          # config
+          ./configuration.nix
+          ./hardware.nix
+          ./modules/avahi.nix
+          ./modules/ssh.nix
+          ./modules/users.nix
+          ./modules/wireguard.nix
+          ./modules/packages.nix
+          ./modules/auto-deploy.nix
+          ./modules/networking.nix
+        ];
+      };
   };
 }
--- hardware.nix
+++ hardware.nix
@@ -1,12 +1,6 @@
 { ... }:
 
 {
-  boot.loader.grub.enable = false;
-  boot.loader.generic-extlinux-compatible.enable = true;
-  boot.kernelParams = [ "console=ttyS1,115200n8" ];
-
-  hardware.raspberry-pi."4".fkms-3d.enable = true;
-
   fileSystems."/" = {
     device = "/dev/disk/by-label/NIXOS_SD";
     fsType = "ext4";
