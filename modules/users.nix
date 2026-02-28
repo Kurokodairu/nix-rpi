8bcbb2586722b60ae07e2cef639a138f08dd7da6
Jørgen <jsvendsli@gmail.com>
2026-02-28 19:28:53 +0000

    smoll

--- modules/users.nix
+++ modules/users.nix
@@ -6,7 +6,6 @@
     extraGroups = [ "wheel" "networkmanager" ];
     shell = pkgs.bash;
     openssh.authorizedKeys.keys = [
-      # REPLACE with your actual public key:
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICXphSQAChtJmZW+yFb3wMf1vK99y/+NqnAsTUHmVY6g jsvendsli@gmail.com"
     ];
   };
