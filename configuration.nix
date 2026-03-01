a8915e8946252ab55339da9391e531d354f15a6f
Jørgen <jsvendsli@gmail.com>
2026-03-01 00:10:27 +0000

    fix warnings for auto-deploy

--- configuration.nix
+++ configuration.nix
@@ -9,6 +9,14 @@
     experimental-features = [ "nix-command" "flakes" ];
     auto-optimise-store = true;
     trusted-users = [ "root" "kuro" ];
+    substituters = [
+    "https://cache.nixos.org"
+    "https://nixos-raspberrypi.cachix.org"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
+    ];
   };
 
   nix.gc = {
